455cc64f61c27fc5a65585f53a41165548266beb
Eduardo de Godoy Ferrari <eduardo.gf@outlook.com.br>
2021-01-31 15:01:37 -0300

    Pega a hash gerada pelo servidor

--- src/main/java/br/com/app/Inicializa.java
+++ src/main/java/br/com/app/Inicializa.java
@@ -4,8 +4,10 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import br.com.app.api.host.RequestMappingEnum;
+import br.com.app.api.request.ConfigRequest;
 import br.com.app.coleta.factory.SystemInfoFactory;
-import br.com.app.util.GenerateHash;
+import br.com.app.model.Config;
 import br.com.app.util.Hash;
 
 @Component
@@ -16,12 +18,11 @@ public class Inicializa {
 
 		String hostName = SystemInfoFactory.createOperatingSystem().getNetworkParams().getDomainName();
 		
-		GenerateHash generateHash = new GenerateHash("SHA-256");
+		ConfigRequest configRequest = new ConfigRequest(RequestMappingEnum.CONFIG_CREATE);
 		
-		String hash = generateHash.gerar(hostName);
+		Config config = configRequest.createConfig(hostName);
 		
-		new Hash().save(hash);
+		new Hash().save(config.getHash());
 	
 	}
-
 }
