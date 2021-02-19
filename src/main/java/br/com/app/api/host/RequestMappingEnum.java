ad0c4c58c63bbe3097ad91c44a5d6fdb69dfc028
Eduardo de Godoy Ferrari <eduardo.gf@outlook.com.br>
2021-02-18 21:28:19 -0300

    Request Mapping

--- src/main/java/br/com/app/api/host/RequestMappingEnum.java
+++ src/main/java/br/com/app/api/host/RequestMappingEnum.java
@@ -3,7 +3,9 @@ package br.com.app.api.host;
 public enum RequestMappingEnum {
 	MEMORY("memory"), PROCESSOR("processor"), SENSORS("sensors"), GRAPHICS_CARDS("graphicscards"),
 	COMPUTER_SYSTEM("computersystem"), DISK_STORES("diskstores"), DISPLAYS("displays"), NETWORKIFS("networkifs"),
-	POWER_SOURCES("powersources"),CONFIG_CREATE("config");
+	POWER_SOURCES("powersources"), CONFIG_CREATE("config"), FILE_SYSTEM("filesystem"),
+	INTERNET_PROTOCOLSTATS("internetprotocolstats"), NETWORK_PARAMS("networkparams"),
+	OPERATING_SYSTEM("operatingsystem"), SERVICES("services"), SESSIONS("sessions"), VERSION_INFO("versioninfo");
 
 	private String value;
 
